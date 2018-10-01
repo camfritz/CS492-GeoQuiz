ff9fc0464645474e371fb3ac5fb3ae5c0f306e1d
Camron Fritz <camfritz@gmail.com>
2018-09-30 19:22:07 -0700

    Add previous question button functionality

--- app/src/main/java/edu/fritzcamoregonstate/fritzcamrongeoquiz/QuizActivity.java
+++ app/src/main/java/edu/fritzcamoregonstate/fritzcamrongeoquiz/QuizActivity.java
@@ -13,6 +13,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -65,6 +66,20 @@ public class QuizActivity extends AppCompatActivity {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 updateQuestion();
             } });
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrentIndex == 0) {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                }
+                else {
+                    mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                }
+                updateQuestion();
+            }
+        });
 
         updateQuestion();
     }
