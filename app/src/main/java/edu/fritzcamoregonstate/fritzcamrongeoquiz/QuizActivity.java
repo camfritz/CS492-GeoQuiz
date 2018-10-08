726faf185b5ef54e06c9286570906af4127c127f
Camron Fritz <camfritz@gmail.com>
2018-10-07 18:18:59 -0700

    Add Intent for starting the CheatActivity when Cheat button is clicked

--- app/src/main/java/edu/fritzcamoregonstate/fritzcamrongeoquiz/QuizActivity.java
+++ app/src/main/java/edu/fritzcamoregonstate/fritzcamrongeoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package edu.fritzcamoregonstate.fritzcamrongeoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -82,7 +83,9 @@ public class QuizActivity extends AppCompatActivity {
         mCheatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Start the CheatActivity
+                Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                startActivity(intent);
             }
         });
 
